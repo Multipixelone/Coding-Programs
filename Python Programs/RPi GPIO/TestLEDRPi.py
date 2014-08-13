4f6690f076f6db71e387fbf82415d31934fc29ff
Tunnel <finn@cnwr.net>
2014-08-13 14:08:38 -0700

    Added a few more LEDs

--- Python Programs/RPi GPIO/TestLEDRPi.py
+++ Python Programs/RPi GPIO/TestLEDRPi.py
@@ -4,6 +4,7 @@ GPIO.setmode(GPIO.BOARD)
 
 	GPIO.setup(18, GPIO.OUT)
 	GPIO.setup(12, GPIO.OUT)
+	GPIO.setup(16, GPIO.OUT)
 
 GPIO.output(18,False)
 
@@ -15,4 +16,6 @@ while True:
 	GPIO.output(12,False)
         time.sleep(1)
 while True:
-	
+	GPIO.output(16,True)
+	time.sleep(2)
+	GPIO.output(16,False)
