7341157bceb633ae46bfa7237c5031b89a258d33
Tunnel <finn@cnwr.net>
2014-08-13 14:44:11 -0700

    Revert 4f6690f..9698a2e
    This rolls back to commit 4f6690f076f6db71e387fbf82415d31934fc29ff.

--- Python Programs/RPi GPIO/TestLEDRPi.py
+++ Python Programs/RPi GPIO/TestLEDRPi.py
@@ -2,9 +2,9 @@ import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BOARD)
 
-GPIO.setup(18, GPIO.OUT)
-GPIO.setup(12, GPIO.OUT)
-GPIO.setup(16, GPIO.OUT)
+	GPIO.setup(18, GPIO.OUT)
+	GPIO.setup(12, GPIO.OUT)
+	GPIO.setup(16, GPIO.OUT)
 
 GPIO.output(18,False)
 
